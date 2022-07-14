fa7455516b91c07b9e298d7715f058fe8f6ca19e
MartynovOleg <103255306+MartynovOleg@users.noreply.github.com>
2022-07-14 11:09:45 +0300

    Initial Commit

---
+++ VKServicesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKServicesApp
+//
+//  Created by mac on 14.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
